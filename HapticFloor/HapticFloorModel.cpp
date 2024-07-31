d87fa108322f2d121b4445e776da311d567849de
HugoGenillier <hgenillier@ensc.fr>
2024-07-31 09:14:35 -0400

    layout file parsing (not tested)

--- HapticFloor/HapticFloorModel.cpp
+++ HapticFloor/HapticFloorModel.cpp
@@ -1,19 +1,57 @@
 #include "HapticFloor.hpp"
 
+
 namespace Example
 {
-void HapticFloor::operator()(halp::tick t)
-{
-  // Process the input buffer
-  for(int i = 0; i < inputs.audio.channels; i++)
-  {
-    auto* in = inputs.audio[i];
-    auto* out = outputs.audio[i];
+void HapticFloor::operator()(){}
 
-    for(int j = 0; j < t.frames; j++)
-    {
-      out[j] = inputs.gain * in[j];
+void HapticFloor::loadLayout()
+{
+  //Reset the layout
+  m_activenodes={};
+  m_passivenodes={};
+
+  try
+  {
+    rapidjson::Document doc;
+    doc.Parse(inputs.layout.value);
+    if(doc.HasParseError())
+      return;
+    if(!doc.IsArray())
+      return;
+
+    for (auto& item : doc.GetArray()) {
+      if (item.HasMember("coords") && item["coords"].IsArray() &&
+         item["coords"].Size() == 2 &&
+         item["coords"][0].IsInt() &&
+         item["coords"][1].IsInt()) {
+
+        int x = item["coords"][0].GetInt();
+        int y = item["coords"][1].GetInt();
+        bool isActive = false;
+        int channel = 0;
+
+        if (item.HasMember("type") && item["type"].IsString()) {
+          std::string type = item["type"].GetString();
+          isActive = (type == "active");
+
+          if (isActive && item.HasMember("channel") && item["channel"].IsInt()) {
+            channel = item["channel"].GetInt();
+          }
+        }
+
+        if (isActive) {
+          m_activenodes.emplace_back(x, y, channel, true);
+        } else {
+          m_passivenodes.emplace_back(x, y);
+        }
+      } else {
+        return;
+      }
     }
+  }
+  catch(...)
+  {
   }
 }
 }
--- HapticFloor/HapticFloorModel.hpp
+++ HapticFloor/HapticFloorModel.hpp
@@ -3,6 +3,9 @@
 #include <halp/audio.hpp>
 #include <halp/controls.hpp>
 #include <halp/meta.hpp>
+#include <rapidjson/document.h>
+#include <rapidjson/filereadstream.h>
+#include <halp/dynamic_port.hpp>
 
 namespace Example
 {
@@ -10,38 +13,44 @@ namespace Example
 class HapticFloor
 {
 public:
-  halp_meta(name, "My Avendish Gain")
-  halp_meta(category, "Audio")
+  halp_meta(name, "Haptic floor")
+  halp_meta(category, "Plugins")
   halp_meta(c_name, "hapticfloor")
+  halp_meta(author, "Hugo Genillier")
   halp_meta(uuid, "2fcacb29-7309-478a-9400-380ded08a64f")
 
-  // Define inputs and outputs ports.
-  // See the docs at https://github.com/celtera/avendish
   struct ins
   {
-    halp::dynamic_audio_bus<"Input", double> audio;
-    halp::knob_f32<"Weight", halp::range{.min = 0., .max = 10., .init = 0.5}> gain;
+    struct : halp::lineedit<"Layout file", "">
+    {
+      halp_meta(language, "json")
+      void update(HapticFloor& self) { self.loadLayout(); }
+    } layout;
+    halp::hslider_f32<"Height of node {}", halp::range{-1, 1, 0}> height;
+
   } inputs;
 
-  struct
+  struct outs
   {
-    halp::dynamic_audio_bus<"Output", double> audio;
+    halp::val_port<"Output", std::vector<float>> out;
   } outputs;
 
-  using setup = halp::setup;
-  void prepare(halp::setup info)
-  {
-    // Initialization, this method will be called with buffer size, etc.
-  }
+  //haptic floor's nodes coordinate system
+  struct node {
+    int x;
+    int y;
+    int channel;
+    bool isActive;
 
-  // Do our processing for N samples
-  using tick = halp::tick;
+    node(int x, int y, int channel = 0, bool isActive = false)
+        : x(x), y(y), channel(channel), isActive(isActive) {}
+  };
+
+  std::vector<node> m_activenodes;
+  std::vector<node> m_passivenodes;
 
   // Defined in the .cpp
-  void operator()(halp::tick t);
-
-  // UI is defined in another file to keep things clear.
-  struct ui;
+  void operator()();
+  void loadLayout();
 };
-
 }
